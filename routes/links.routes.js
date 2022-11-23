8e5a073136a1746d8e42b26179de3dedcb4e4d57
emilykpritchard <emilykpritchard@me.com>
2022-11-23 11:27:19 +0000

    Carousel started but needs a lot of work

--- routes/links.routes.js
+++ routes/links.routes.js
@@ -19,8 +19,7 @@ linksRouter.get("/", async function (req, res) {
   res.status(200).json({
     success: true,
     payload: links,
-  });
-});
+  })});
 
 linksRouter.post("/", async function (req, res) {
   const somethingIsMissing =
@@ -41,48 +40,3 @@ linksRouter.post("/", async function (req, res) {
     payload: created,
   });
 });
-
-
-/*
-todosRouter.post("/", async function (req, res) {
-  const somethingIsMissing =
-  req.body.link === undefined || req.body.title === undefined || req.body.description === undefined || req.body.week === undefined || req.body.subject === undefined;
-
-  if (somethingIsMissing) {
-    res.status(400).json({
-      success: false,
-      error: "Please provide all fields for link to be added",
-    });
-    return;
-  }
-
-  const created = await linksModel.createLink(req.body);
-
-  res.status(201).json({
-    success: true,
-    payload: created,
-  });
-});
-/*
-
-todosRouter.delete("/:id", async function (req, res) {
-  const deleted = await todosModel.deleteTodoById(req.params.id);
-
-  res.status(200).json({
-    success: true,
-    payload: deleted,
-  });
-});
-
-
-todosRouter.patch("/:id", async function (req, res){
-  const updated = await todosModel.updateById(req.params.id,req.body);
-  console.log(updated)
-    res.status(200).json({
-    success: true,
-    payload: updated,
-    
-
-  });
-
-});*/
